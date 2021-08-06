72023182ae606633f29ea7f471daff147b9edb0a
Trisha Le <trishaprilee@gmail.com>
2021-08-06 15:57:12 -0700

    version 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-import NavigationBar from './components/navbar';
-import Cover from './components/cover';
-import About from './components/about';
-import Experience from './components/experience';
-import Work from './components/work';
-import Contact from './components/contact';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavigationBar />
-        <Cover />
-        <About />
-        <Experience />
-        <Work />
-        <Contact />
-      </div>
-    );
-  }
+const App = () => { 
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
 }
 export default App;
