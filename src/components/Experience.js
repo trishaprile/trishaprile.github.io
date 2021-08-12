72bcd64c04394d9ff2a686f1781b084e9a19dd40
Trisha Le <trishaprilee@gmail.com>
2021-08-11 18:47:14 -0700

    update toolbox

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './Experience.scss';
 
-import { Row, Col, Image, Button } from "react-bootstrap";
-import figma from "../assets/figma.png";
-import adobecc from "../assets/adobecc.png";
-import bootstrap from "../assets/bootstrap.png";
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import javascript from "../assets/javascript.png";
-import react from "../assets/react.png";
-import angular from "../assets/angular.png";
-import nodejs from "../assets/nodejs.png";
-import django from "../assets/django.jpg";
-import graphql from "../assets/graphql.png";
-import git from "../assets/git.png";
+import { Row, Col, Image, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import figma from "../assets/toolbox_icons/figma.png";
+import adobecc from "../assets/toolbox_icons/adobecc.png";
+import bootstrap from "../assets/toolbox_icons/bootstrap.png";
+import html from "../assets/toolbox_icons/html.png";
+import css from "../assets/toolbox_icons/css.png";
+import javascript from "../assets/toolbox_icons/javascript.png";
+import react from "../assets/toolbox_icons/react.png";
+import angular from "../assets/toolbox_icons/angular.png";
+import django from "../assets/toolbox_icons/django.png";
+import graphql from "../assets/toolbox_icons/graphql.png";
+import docker from "../assets/toolbox_icons/docker.png";
+import git from "../assets/toolbox_icons/git.png";
 import resume from '../assets/Trisha Le Resume.pdf';
 
 const Experience = () => { 
@@ -49,24 +49,108 @@ const Experience = () => {
           <h1>toolbox</h1>
           <div className="toolbox">
             <Row xs={3}>
-              <Col className="icon-container"><Image className="icon" src={figma} alt="Figma"/></Col>
-              <Col className="icon-container"><Image className="icon" src={adobecc} alt="Adobe Creative Cloud"/></Col>
-              <Col className="icon-container"><Image className="icon" src={bootstrap} alt="Bootstrap"/></Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Figma</Tooltip>}
+                >
+                  <Image className="icon" src={figma} alt="Figma"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Adobe Creative Cloud</Tooltip>}
+                >
+                  <Image className="icon" src={adobecc} alt="Adobe Creative Cloud"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Bootstrap</Tooltip>}
+                >
+                  <Image className="icon" src={bootstrap} alt="Bootstrap"/>
+                </OverlayTrigger>
+              </Col>
             </Row>
             <Row xs={3}>
-              <Col className="icon-container"><Image className="icon" src={html} alt="HTML"/></Col>
-              <Col className="icon-container"><Image className="icon" src={css} alt="CSS"/></Col>
-              <Col className="icon-container"><Image className="icon" src={javascript} alt="Javascript"/></Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>HTML</Tooltip>}
+                >
+                  <Image className="icon" src={html} alt="HTML"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>CSS</Tooltip>}
+                >
+                  <Image className="icon" src={css} alt="CSS"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>JavaScript</Tooltip>}
+                >
+                  <Image className="icon" src={javascript} alt="JavaScript"/>
+                </OverlayTrigger>
+              </Col>
             </Row>
             <Row xs={3}>
-              <Col className="icon-container"><Image className="icon" src={react} alt="React"/></Col>
-              <Col className="icon-container"><Image className="icon" src={angular} alt="Angular"/></Col>
-              <Col className="icon-container"><Image className="icon" src={nodejs} alt="NodeJS"/></Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>React</Tooltip>}
+                >
+                  <Image className="icon" src={react} alt="React"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Angular</Tooltip>}
+                >
+                  <Image className="icon" src={angular} alt="Angular"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Django</Tooltip>}
+                >
+                  <Image className="icon" src={django} alt="Django"/>
+                </OverlayTrigger>
+              </Col>
             </Row>
             <Row xs={3}>
-              <Col className="icon-container"><Image className="icon" src={django} alt="Django"/></Col>
-              <Col className="icon-container"><Image className="icon" src={graphql} alt="GraphQL"/></Col>
-              <Col className="icon-container"><Image className="icon" src={git} alt="Git"/></Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>GraphQL</Tooltip>}
+                >
+                  <Image className="icon" src={graphql} alt="GraphQL"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Docker</Tooltip>}
+                >
+                  <Image className="icon" src={docker} alt="Docker"/>
+                </OverlayTrigger>
+              </Col>
+              <Col className="icon-container">
+                <OverlayTrigger
+                  placement="bottom"
+                  overlay={<Tooltip>Git</Tooltip>}
+                >
+                  <Image className="icon" src={git} alt="Git"/>
+                </OverlayTrigger>
+              </Col>
             </Row>
           </div>
           <Button className="resume-btn" href={resume} target="_blank" rel="noreferrer">VIEW RESUME</Button>
