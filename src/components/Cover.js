a9e2fc97c00823d9618cb1f8bed251e9f3b6ce65
Trisha Le <trishaprilee@gmail.com>
2021-08-13 17:16:18 -0700

    update styling

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -3,6 +3,7 @@ import './Cover.scss';
 
 import { Row, Col, Button, Image } from "react-bootstrap";
 import { Element } from 'react-scroll';
+import { scroller } from 'react-scroll';
 
 import img  from "../assets/cover_img.svg";
 
@@ -14,7 +15,16 @@ const Cover = () => {
           <h1>Hello!</h1>
           <h2>I'm Trisha,</h2>
           <h3>a designer and developer who enjoys creating impactful experiences, from ideation to execution.</h3>
-          <Button className="contact-btn" href="#contact">LET'S CHAT!</Button>
+          <Button 
+            className="contact-btn"
+            onClick={() => scroller.scrollTo('contact', {
+              smooth: true,
+              offset: -50,
+              duration: 500,
+            })}
+          >
+            LET'S CHAT!
+          </Button>
         </Col>
         <Col className="img-container" sm={12} md={8} xl={6}>
           <Image src={img} className="img" />
