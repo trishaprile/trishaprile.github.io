3d0d603dea85a59e4bd41581f29e2f134de5c829
Trisha Le <trishaprilee@gmail.com>
2022-05-13 22:09:20 -0700

    version 2!

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.scss';
 
-import NavigationBar from "./components/Navbar";
-import Cover from "./components/Cover";
-import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-
 const App = () => {
   return (
     <div className="App">
-      <NavigationBar />
-      <Cover />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
+      <div className="info">
+        <h1>currently under construction</h1>
+        <p>come back soon!</p>
+      </div>
     </div>
   );
 }
